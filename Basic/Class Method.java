2635d56862ec3fd7a1dc9f0d3bcc23d5a542e86c
Abinav <abinavdharunav@gmail.com>
2025-09-19 11:46:40 +0530

    Added Class Method.java

---
+++ Basic/Class Method.java
@@ -0,0 +1,21 @@
+class Calculator
+{
+    int add(int num1 , int num2)
+    {
+        return num1 + num2;
+    }
+}
+
+class Demo
+{
+    public static void main(String args[])
+    {
+        int num1 = 5;
+        int num2 = 10;
+        
+        Calculator calc = new Calculator();
+
+        int result = calc.add(num1 , num2);
+        System.out.println("The sum is: " + result);
+    }
+}
